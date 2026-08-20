23f955295e6352597ff82aa3d871deb80b888753
Apoorv Agarwal <150249267+Apoorv-AK-Coder@users.noreply.github.com>
2026-08-20 18:47:13 +0530

    changes

--- css/file.js
+++ css/file.js
@@ -27,11 +27,11 @@ navLinks.forEach(link => {
   }
 });
 
-window.addEventListener('scroll', () => {
-  const header = document.querySelector('header');
-  if (window.scrollY > 100) {
-    header.classList.add('scrolled');
-  } else {
-    header.classList.remove('scrolled');
-  }
-});
+// window.addEventListener('scroll', () => {
+//   const header = document.querySelector('header');
+//   if (window.scrollY > 100) {
+//     header.classList.add('scrolled');
+//   } else {
+//     header.classList.remove('scrolled');
+//   }
+// });
